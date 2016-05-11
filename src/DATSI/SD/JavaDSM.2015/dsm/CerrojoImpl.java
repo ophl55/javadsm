be95a1cde3ee52522e75132ddb7b4bc7bdb4cfaa
Alokaado <temesgen.ks.mehari@gmail.com>
2016-05-11 22:22:21 +0200

    both functiones of phase 1 commented

--- src/DATSI/SD/JavaDSM.2015/dsm/CerrojoImpl.java
+++ src/DATSI/SD/JavaDSM.2015/dsm/CerrojoImpl.java
@@ -4,12 +4,80 @@ import java.rmi.server.*;
 
 class CerrojoImpl extends UnicastRemoteObject implements Cerrojo {
 
+    private static final long serialVersionUID = 1L;//for synchronized methods
+    boolean writer;
+    int readers;
     CerrojoImpl() throws RemoteException {
+        this.writer     = true;
+        this.readers    = 0;
     }
 
+    /**
+     * function to adquire the associated object to the lock
+     * there can be more than one reader but just one writer
+     * @param exc says if access is (exclusive) write access
+     * @throws RemoteException
+     */
     public synchronized void adquirir (boolean exc) throws RemoteException {
+
+        while (true) {
+            try {
+                //write access
+                if (exc) {
+                    //if there are no writer and readers
+                    if (!writer && readers == 0) {
+                        writer = true;//set writer
+                        return;
+                    }
+                    //else next writer has to wait
+                    else
+                        wait();
+
+                }
+
+                //read access with current writer writing
+                else if (writer)
+                    wait();//reader has to wait
+
+                    //read access with just readers accessing on object
+                else {
+                    readers++;//increment readers
+                    return;
+                }
+
+            }
+            //exception for thread interrupts on waiting thread
+            catch(InterruptedException e){
+                e.printStackTrace();//print status
+            }
+
+        }
     }
+
+    /**
+     * frees the set lock
+     * @return true if the free was successfull, false otherwise
+     * @throws RemoteException
+     */
     public synchronized boolean liberar() throws RemoteException {
-        return true;
+
+        //if there are no current writer or reader there must had been an error
+        if (!writer && readers == 0)
+            return false;
+        //if there are reader
+        if (readers > 0) {
+            readers--;//free a reader
+            //if there are no reader anymore
+            if (readers == 0) {
+                notify();//notify the waiting writer process
+                return true;
+            }
+        }
+        //else there must be a writer
+        else {
+            writer = false;//free the writer
+            notify();//notify the next writer or reader
+            return true;
+        }
+        return true;//programm never comes to this point
     }
-}
--- src/DATSI/SD/JavaDSM.2015/dsm/FabricaCerrojosImpl.java
+++ src/DATSI/SD/JavaDSM.2015/dsm/FabricaCerrojosImpl.java
@@ -2,13 +2,34 @@ package dsm;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FabricaCerrojosImpl extends UnicastRemoteObject implements FabricaCerrojos {
 
+    /*Hashmap, which maps locks to their names (given as Strings) as keys*/
+    Map<String, Cerrojo> locks = new HashMap<String, Cerrojo> ();
+
     public FabricaCerrojosImpl() throws RemoteException {
     }
+
+    /**
+     * returns a lock with name s, if it already exists, it will be returned,
+     * otherwise the function creates a new lock and returns it
+     * @param s=name of the lock given as String
+     * @return a lock (class Cerrojo) which has the name s
+     * @throws RemoteException
+     */
     public synchronized	Cerrojo iniciar(String s) throws RemoteException {
-	return null;
+
+        //if the lock already exists, return it
+	    if(locks.containsKey(s))
+            return locks.get(s);
+
+        Cerrojo newlock = new CerrojoImpl();//create new lock
+        locks.put(s,newlock);//save lock in map
+
+        return newlock;
     }
 }
 
